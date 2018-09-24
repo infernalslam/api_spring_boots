87504da963f5319bf71fd0902269f9b8d6de3111
Kamonpob <kamonpob@TH-LW11327-m.local>
2018-09-24 16:27:53 +0700

    init project api

---
+++ src/main/java/com/example/demo_spring_api/DemoSpringApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo_spring_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringApiApplication.class, args);
+	}
+}
