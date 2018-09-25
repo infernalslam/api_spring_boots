f7e500752fb73296af2d611e1025ccd88b52e8e1
Kamonpob <kamonpob@TH-LW11327-m.local>
2018-09-25 14:25:31 +0700

    fix code

--- src/main/java/com/example/demo_spring_api/controllers/ClientController.java
+++ src/main/java/com/example/demo_spring_api/controllers/ClientController.java
@@ -3,9 +3,7 @@ package com.example.demo_spring_api.controllers;
 import com.example.demo_spring_api.models.Quote;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -17,5 +15,15 @@ public class ClientController {
         RestTemplate restTemplate = new RestTemplate();
         Quote quote = restTemplate.getForObject(uri, Quote.class);
         return  new ResponseEntity<>(quote, HttpStatus.OK);
+    }
+//    Simple validation api: http://localhost:3000/api/user/tak
+    @GetMapping("/user/{name}")
+    public String getUser (@PathVariable(value = "name", required = true) String name) {
+        return  name;
+    }
+//    simple validation api: http://localhost:3000/api/admin?name=tak
+    @GetMapping("/admin")
+    public ResponseEntity<?> queryPerson(@RequestParam(value = "name", required = false) String query) {
+        return new ResponseEntity<>(query, HttpStatus.OK);
     }
 }
